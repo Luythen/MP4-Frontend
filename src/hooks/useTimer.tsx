0c9a7b0fe9adddc7056d71d37ebdc50bc349b04b
Luythen <andenklasson@gmail.com>
2026-09-08 09:50:49 +0200

    add/useTimer

---
+++ src/hooks/useTimer.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react"
+import { subscribe, unSubscribe } from "../stomp"
+
+export default function useTimer () {
+    const [timer, setTimer] = useState<number>(0)
+    const subscribePath = "/topic/send-timer"
+
+    useEffect(() => {
+        subscribe((t: number) => {
+            setTimer(t)
+        }, subscribePath)
+
+        return () => {
+            unSubscribe(subscribePath);
+            console.log("unsubscribing to timer");
+            
+        }
+    })
+    return { timer };
+}
